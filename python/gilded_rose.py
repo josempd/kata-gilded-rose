b7bb8d1761b217e37859e09b44cb54a4975187bb
josempd <josempd@gmail.com>
2023-09-16 21:09:47 +0200

    refactorized kata aiming to follow SOLID principles

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from typing import List
 
 class GildedRose(object):
@@ -8,34 +7,22 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            classified_item = self._item_classification(item)
+            classified_item.update()
+            item.sell_in = classified_item.sell_in
+            item.quality = classified_item.quality
+                    
+    def _item_classification(self, item):
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            return LegendaryItem(item.name, item.sell_in, item.quality)
+        elif "Conjured" in item.name:
+            return ConjuredItem(item.name, item.sell_in, item.quality)
+        elif "Aged" in item.name:
+            return AgedItem(item.name, item.sell_in, item.quality)
+        elif "concert" in item.name:
+            return ConcertItem(item.name, item.sell_in, item.quality)
+        else:
+            return GenericItem(item.name, item.sell_in, item.quality) 
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -45,3 +32,67 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+class GenericItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        self.sell_in = self.sell_in - 1
+        if self.sell_in < 0:
+            self.quality = self.quality -1
+            self.quality = self.quality -1
+        else:
+            self.quality = self.quality - 1
+        if self.quality < 0:
+            self.quality = 0
+
+class ConjuredItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        self.sell_in = self.sell_in - 1
+        if self.sell_in < 0:
+            self.quality = self.quality -1
+            self.quality = self.quality -1
+            self.quality = self.quality -1
+            self.quality = self.quality -1    
+        else:
+            self.quality = self.quality - 1
+            self.quality = self.quality - 1
+        if self.quality < 0:
+            self.quality = 0
+    
+class LegendaryItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+       self.quality = 80
+    
+class AgedItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        self.sell_in = self.sell_in - 1
+        if self.quality < 50:
+            self.quality = self.quality + 1
+            if self.sell_in < 0:
+                self.quality = self.quality + 1
+    
+class ConcertItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        self.sell_in = self.sell_in - 1
+        if self.quality < 50:
+            self.quality = self.quality + 1
+            if self.sell_in < 11:
+                self.quality = self.quality + 1
+            if self.sell_in < 6:
+                self.quality = self.quality + 1
+            if self.sell_in < 0:
+                self.quality = 0
